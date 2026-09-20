58fb142fc8006b62c08693922859cb5630176788
Omega <emomega@proton.me>
2026-09-20 18:43:26 -0400

    Add new modules to the library

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,18 @@
 //! The library contains the shared core used by Conduit's GUI, TUI,
 //! and CLI interfaces.
 
+pub mod config_engine;
 pub mod core;
-
+pub mod events;
+pub mod gui;
+pub mod live;
+pub mod panes;
+pub mod resources;
+pub mod tabs;
+pub mod tui;
+pub mod windows;
+pub mod workspaces;
+pub mod colors;
 pub use core::{
     Process,
     ProcessConfig,
