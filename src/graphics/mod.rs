29b1588a84453b9dd1443a70830dfb1c00e88f81
Omega <emomega@proton.me>
2026-09-20 23:12:29 -0400

    Create mod.rs

---
+++ src/graphics/mod.rs
@@ -0,0 +1,48 @@
+pub mod image;
+pub mod iterm2;
+pub mod kitty;
+pub mod preview;
+pub mod protocols;
+pub mod scaling;
+pub mod sixel;
+
+pub use image::{
+    Image,
+    ImageData,
+    ImageFormat,
+    ImageId,
+    ImageMetadata,
+};
+
+pub use iterm2::{
+    Iterm2Image,
+    Iterm2ImageDecoder,
+};
+
+pub use kitty::{
+    KittyImage,
+    KittyImageDecoder,
+    KittyImageFormat,
+};
+
+pub use preview::{
+    ImagePreview,
+    PreviewOptions,
+};
+
+pub use protocols::{
+    GraphicsCommand,
+    GraphicsProtocol,
+    GraphicsProtocolKind,
+};
+
+pub use scaling::{
+    ImageScaleMode,
+    ImageScaler,
+    ScaleDimensions,
+};
+
+pub use sixel::{
+    SixelImage,
+    SixelImageDecoder,
+};
