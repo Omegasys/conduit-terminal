b4167e1e4087890e3da47e0bcfee74ab4c856e9f
Omega <emomega@proton.me>
2026-09-20 23:22:22 -0400

    Create podman.rs

---
+++ src/connections/podman.rs
@@ -0,0 +1,84 @@
+use super::containers::{
+    ContainerConnection,
+    ContainerId,
+    ContainerRuntime,
+};
+
+use super::manager::{
+    Connection,
+    ConnectionId,
+    ConnectionState,
+    ConnectionType,
+};
+
+#[derive(Debug, Clone)]
+pub struct PodmanContainer {
+    pub id: ContainerId,
+    pub name: String,
+    pub image: String,
+    pub running: bool,
+}
+
+impl PodmanContainer {
+    pub fn new(
+        id: impl Into<String>,
+        name: impl Into<String>,
+        image: impl Into<String>,
+    ) -> Self {
+        Self {
+            id: ContainerId::new(id),
+            name: name.into(),
+            image: image.into(),
+            running: false,
+        }
+    }
+}
+
+pub struct PodmanConnection {
+    inner: ContainerConnection,
+}
+
+impl PodmanConnection {
+    pub fn new(
+        id: ConnectionId,
+        container_id: impl Into<String>,
+    ) -> Self {
+        Self {
+            inner: ContainerConnection::new(
+                id,
+                ContainerId::new(container_id),
+                ContainerRuntime::Podman,
+            ),
+        }
+    }
+
+    pub fn container(&self) -> &ContainerId {
+        self.inner.container_id()
+    }
+
+    pub fn into_inner(self) -> ContainerConnection {
+        self.inner
+    }
+}
+
+impl Connection for PodmanConnection {
+    fn id(&self) -> ConnectionId {
+        self.inner.id()
+    }
+
+    fn connection_type(&self) -> ConnectionType {
+        ConnectionType::Podman
+    }
+
+    fn state(&self) -> ConnectionState {
+        self.inner.state()
+    }
+
+    fn connect(&mut self) -> Result<(), String> {
+        self.inner.connect()
+    }
+
+    fn disconnect(&mut self) -> Result<(), String> {
+        self.inner.disconnect()
+    }
+}
