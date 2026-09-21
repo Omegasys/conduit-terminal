b4825674b66452207838db40145fc1c1fce8a0ba
Omega <emomega@proton.me>
2026-09-20 22:59:15 -0400

    Refactor CustomProtocol to CustomTerminalProtocol

--- src/terminal_protocols/custom/registry.rs
+++ src/terminal_protocols/custom/registry.rs
@@ -1,108 +1,158 @@
 use std::collections::HashMap;
 
 use super::{
-    errors::{CustomProtocolError, CustomProtocolResult},
-    protocol::{CustomTerminalProtocol, ProtocolId},
+    errors::{
+        CustomProtocolError,
+        CustomProtocolResult,
+    },
+    protocol::{
+        CustomProtocol,
+        CustomProtocolId,
+    },
 };
 
 pub struct ProtocolRegistration {
-    pub protocol: Box<dyn CustomTerminalProtocol>,
-    pub enabled: bool,
+    protocol: Box<dyn CustomProtocol>,
+    enabled: bool,
 }
 
 impl ProtocolRegistration {
-    pub fn new(protocol: Box<dyn CustomTerminalProtocol>) -> Self {
+    pub fn new(
+        protocol: Box<dyn CustomProtocol>,
+    ) -> Self {
         Self {
             protocol,
             enabled: true,
         }
     }
 
-    pub fn id(&self) -> ProtocolId {
+    pub fn protocol(&self) -> &dyn CustomProtocol {
+        self.protocol.as_ref()
+    }
+
+    pub fn protocol_mut(
+        &mut self,
+    ) -> &mut dyn CustomProtocol {
+        self.protocol.as_mut()
+    }
+
+    pub fn id(&self) -> CustomProtocolId {
         self.protocol.id()
     }
 
-    pub fn disable(&mut self) {
-        self.enabled = false;
+    pub fn enabled(&self) -> bool {
+        self.enabled
     }
 
     pub fn enable(&mut self) {
         self.enabled = true;
     }
+
+    pub fn disable(&mut self) {
+        self.enabled = false;
+    }
 }
 
+#[derive(Default)]
 pub struct CustomProtocolRegistry {
-    protocols: HashMap<ProtocolId, ProtocolRegistration>,
+    protocols: HashMap<
+        CustomProtocolId,
+        ProtocolRegistration,
+    >,
 }
 
 impl CustomProtocolRegistry {
     pub fn new() -> Self {
-        Self {
-            protocols: HashMap::new(),
-        }
+        Self::default()
     }
 
     pub fn register(
         &mut self,
-        protocol: Box<dyn CustomTerminalProtocol>,
+        protocol: Box<dyn CustomProtocol>,
     ) -> CustomProtocolResult<()> {
         let id = protocol.id();
 
         if self.protocols.contains_key(&id) {
-            return Err(CustomProtocolError::AlreadyRegistered(id.to_string()));
+            return Err(CustomProtocolError::AlreadyRegistered(
+                id.to_string(),
+            ));
         }
 
-        self.protocols.insert(id, ProtocolRegistration::new(protocol));
+        self.protocols.insert(
+            id,
+            ProtocolRegistration::new(protocol),
+        );
 
         Ok(())
     }
 
     pub fn unregister(
         &mut self,
-        id: &ProtocolId,
-    ) -> CustomProtocolResult<()> {
-        self.protocols
-            .remove(id)
-            .map(|_| ())
-            .ok_or_else(|| CustomProtocolError::NotFound(id.to_string()))
+        id: &CustomProtocolId,
+    ) -> Option<ProtocolRegistration> {
+        self.protocols.remove(id)
     }
 
     pub fn get(
         &self,
-        id: &ProtocolId,
+        id: &CustomProtocolId,
     ) -> Option<&ProtocolRegistration> {
         self.protocols.get(id)
     }
 
     pub fn get_mut(
         &mut self,
-        id: &ProtocolId,
+        id: &CustomProtocolId,
     ) -> Option<&mut ProtocolRegistration> {
         self.protocols.get_mut(id)
     }
 
-    pub fn contains(&self, id: &ProtocolId) -> bool {
-        self.protocols.contains_key(id)
-    }
+    pub fn enable(
+        &mut self,
+        id: &CustomProtocolId,
+    ) -> CustomProtocolResult<()> {
+        let protocol = self
+            .get_mut(id)
+            .ok_or_else(|| {
+                CustomProtocolError::NotFound(
+                    id.to_string(),
+                )
+            })?;
 
-    pub fn enable(&mut self, id: &ProtocolId) -> CustomProtocolResult<()> {
-        self.get_mut(id)
-            .ok_or_else(|| CustomProtocolError::NotFound(id.to_string()))?
-            .enable();
-
-        Ok(())
-    }
-
-    pub fn disable(&mut self, id: &ProtocolId) -> CustomProtocolResult<()> {
-        self.get_mut(id)
-            .ok_or_else(|| CustomProtocolError::NotFound(id.to_string()))?
-            .disable();
+        protocol.enable();
 
         Ok(())
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = &ProtocolRegistration> {
+    pub fn disable(
+        &mut self,
+        id: &CustomProtocolId,
+    ) -> CustomProtocolResult<()> {
+        let protocol = self
+            .get_mut(id)
+            .ok_or_else(|| {
+                CustomProtocolError::NotFound(
+                    id.to_string(),
+                )
+            })?;
+
+        protocol.disable();
+
+        Ok(())
+    }
+
+    pub fn iter(
+        &self,
+    ) -> impl Iterator<Item = &ProtocolRegistration> {
         self.protocols.values()
+    }
+
+    pub fn iter_enabled(
+        &self,
+    ) -> impl Iterator<Item = &ProtocolRegistration> {
+        self.protocols
+            .values()
+            .filter(|protocol| protocol.enabled())
     }
 
     pub fn len(&self) -> usize {
@@ -115,11 +165,5 @@ impl CustomProtocolRegistry {
 
     pub fn clear(&mut self) {
         self.protocols.clear();
-    }
-}
-
-impl Default for CustomProtocolRegistry {
-    fn default() -> Self {
-        Self::new()
     }
 }
