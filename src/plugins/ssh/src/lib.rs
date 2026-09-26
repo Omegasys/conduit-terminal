a1485ad219f6988d25087bcd33b7bf6cf282eb7c
Omega <emomega@proton.me>
2026-09-26 17:07:22 -0400

    Add SSH integration plugin for Conduit

---
+++ src/plugins/ssh/src/lib.rs
@@ -0,0 +1,96 @@
+use conduit_plugin_api::{
+    Plugin,
+    PluginContext,
+    PluginEvent,
+    PluginId,
+    PluginResult,
+};
+
+/// SSH integration plugin for Conduit.
+pub struct SshPlugin {
+    id: PluginId,
+    enabled: bool,
+}
+
+impl Default for SshPlugin {
+    fn default() -> Self {
+        Self {
+            id: PluginId::new("ssh"),
+            enabled: true,
+        }
+    }
+}
+
+impl SshPlugin {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    fn handle_custom_event(
+        &mut self,
+        name: &str,
+        _data: &[u8],
+        _context: &mut PluginContext,
+    ) -> PluginResult {
+        match name {
+            "ssh.connect" => Ok(()),
+            "ssh.disconnect" => Ok(()),
+            "ssh.host.changed" => Ok(()),
+            _ => Ok(()),
+        }
+    }
+}
+
+impl Plugin for SshPlugin {
+    fn id(&self) -> &PluginId {
+        &self.id
+    }
+
+    fn initialize(
+        &mut self,
+        context: &mut PluginContext,
+    ) -> PluginResult {
+        self.enabled = context
+            .configuration()
+            .get("enabled")
+            .map(|value| value != "false")
+            .unwrap_or(true);
+
+        Ok(())
+    }
+
+    fn handle_event(
+        &mut self,
+        event: &PluginEvent,
+        context: &mut PluginContext,
+    ) -> PluginResult {
+        if !self.enabled {
+            return Ok(());
+        }
+
+        match event {
+            PluginEvent::Custom { name, data } => {
+                self.handle_custom_event(name, data, context)
+            }
+
+            PluginEvent::PaneCreated { .. }
+            | PluginEvent::SessionCreated { .. }
+            | PluginEvent::ConfigurationChanged => Ok(()),
+
+            _ => Ok(()),
+        }
+    }
+
+    fn shutdown(
+        &mut self,
+        _context: &mut PluginContext,
+    ) -> PluginResult {
+        self.enabled = false;
+        Ok(())
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn conduit_plugin_create() -> *mut dyn Plugin {
+    Box::into_raw(Box::new(SshPlugin::new()))
+}
