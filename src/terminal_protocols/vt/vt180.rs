9a8b8c5456185c19441d4e7cb9089c146d9ec21b
Omega <emomega@proton.me>
2026-09-25 23:03:48 -0400

    Add Vt180 struct with capabilities and level methods

---
+++ src/terminal_protocols/vt/vt180.rs
@@ -0,0 +1,18 @@
+use super::{Vt100, VtCapabilities, VtLevel};
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Vt180;
+
+impl Vt180 {
+    pub fn level(&self) -> VtLevel {
+        VtLevel::Vt180
+    }
+
+    pub fn capabilities(&self) -> VtCapabilities {
+        VtCapabilities::for_level(self.level())
+    }
+
+    pub fn base(&self) -> Vt100 {
+        Vt100
+    }
+}
