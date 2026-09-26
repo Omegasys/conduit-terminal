529aad460b77fa833633d04c29c3fdc3c2a4fb71
Omega <emomega@proton.me>
2026-09-26 17:07:50 -0400

    Add DockerPlugin for Conduit integration
    Implement Docker integration plugin for Conduit with event handling.

---
+++ src/plugins/docker/src/lib.rs
@@ -0,0 +1,98 @@
+use conduit_plugin_api::{
+    Plugin,
+    PluginContext,
+    PluginEvent,
+    PluginId,
+    PluginResult,
+};
+
+/// Docker integration plugin for Conduit.
+pub struct DockerPlugin {
+    id: PluginId,
+    enabled: bool,
+}
+
+impl Default for DockerPlugin {
+    fn default() -> Self {
+        Self {
+            id: PluginId::new("docker"),
+            enabled: true,
+        }
+    }
+}
+
+impl DockerPlugin {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    fn handle_custom_event(
+        &mut self,
+        name: &str,
+        _data: &[u8],
+        _context: &mut PluginContext,
+    ) -> PluginResult {
+        match name {
+            "docker.container.created" => Ok(()),
+            "docker.container.started" => Ok(()),
+            "docker.container.stopped" => Ok(()),
+            "docker.container.removed" => Ok(()),
+            "docker.image.updated" => Ok(()),
+            _ => Ok(()),
+        }
+    }
+}
+
+impl Plugin for DockerPlugin {
+    fn id(&self) -> &PluginId {
+        &self.id
+    }
+
+    fn initialize(
+        &mut self,
+        context: &mut PluginContext,
+    ) -> PluginResult {
+        self.enabled = context
+            .configuration()
+            .get("enabled")
+            .map(|value| value != "false")
+            .unwrap_or(true);
+
+        Ok(())
+    }
+
+    fn handle_event(
+        &mut self,
+        event: &PluginEvent,
+        context: &mut PluginContext,
+    ) -> PluginResult {
+        if !self.enabled {
+            return Ok(());
+        }
+
+        match event {
+            PluginEvent::Custom { name, data } => {
+                self.handle_custom_event(name, data, context)
+            }
+
+            PluginEvent::PaneCreated { .. }
+            | PluginEvent::SessionCreated { .. }
+            | PluginEvent::ConfigurationChanged => Ok(()),
+
+            _ => Ok(()),
+        }
+    }
+
+    fn shutdown(
+        &mut self,
+        _context: &mut PluginContext,
+    ) -> PluginResult {
+        self.enabled = false;
+        Ok(())
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn conduit_plugin_create() -> *mut dyn Plugin {
+    Box::into_raw(Box::new(DockerPlugin::new()))
+}
